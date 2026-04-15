6edb8829d77e792fb8dc68a83276598b94c4d9a8
PhantomOblivion <smasif404@gmail.com>
2026-04-15 16:00:48 +0600

    SummaryCards and Friends file has been created.Working on Progress.

---
+++ src/components/friends/Friends.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Your Friends Section Will be here</h2>
+        </div>
+    );
+};
+
+export default Friends;
---
+++ src/components/summaryCard/SummaryCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SummaryCards = () => {
+    return (
+        <div>
+            <h2>SummaryCards Section Will be here</h2>
+        </div>
+    );
+};
+
+export default SummaryCards;
