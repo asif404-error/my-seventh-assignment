8439063617342f8d89d83580cd7e5288d6a9f264
PhantomOblivion <smasif404@gmail.com>
2026-04-16 01:09:03 +0600

    freindDeatils folder and file opened. Work in progress...

---
+++ src/components/friendDetails/FriendDetailsL.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetailsL = () => {
+    return (
+        <div>
+            <h2>FriendDetails Left Side Will be Visible Here</h2>
+        </div>
+    );
+};
+
+export default FriendDetailsL;
---
+++ src/components/friendDetails/FriendDetailsR.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendDetailsR = () => {
+  return (
+    <div>
+      <h2>Right Side FriendDetails will be visible here</h2>
+    </div>
+  );
+};
+
+export default FriendDetailsR;
