94f185dc0594e8a86ccd283f28193ce92c46e474
PhantomOblivion <smasif404@gmail.com>
2026-04-15 16:10:21 +0600

    Banner, SummaryCard, Friends section connected to Home. Outlet added to MainLayout and main.jsx also updated.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import "./index.css";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Banner from '../components/banner/Banner';
+import SummaryCards from '../components/summaryCard/SummaryCards';
+import Friends from '../components/friends/Friends';
 
 const Home = () => {
     return (
         <div>
             <h2>This will be the Homepage</h2>
+            <Banner />
+            <SummaryCards />
+            <Friends />
         </div>
     );
 };
