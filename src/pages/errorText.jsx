286f65f0787e408590249e43d789e7852fd9c124
PhantomOblivion <smasif404@gmail.com>
2026-04-16 16:24:51 +0600

    Error component has made under the page folder.To be Continued...

---
+++ src/pages/errorText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const errorText = () => {
+    return (
+        <div>
+            <h2>Error Text Preview Will be Visible here</h2>
+        </div>
+    );
+};
+
+export default errorText;
