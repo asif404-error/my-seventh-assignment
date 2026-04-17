1960b80e91bdb1f07791f9da211712a40690bd19
PhantomOblivion <smasif404@gmail.com>
2026-04-17 21:19:55 +0600

    Made Changes to Responsive Looks Better.

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -3,10 +3,11 @@ import FriendCard from "../friendCard/FriendCard";
 
 const Friends = ({ friends }) => {
   return (
-    <div className="px-4">
-      <h2 className="text-xl font-semibold mb-4">Your Friends</h2>
-
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+    <div className="px-4 md:px-6 lg:px-8">
+      <h2 className="text-lg sm:text-xl md:text-2xl font-semibold mb-4">
+        Your Friends
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-5">
         {friends.map((friend) => (
           <FriendCard key={friend.id} friend={friend} />
         ))}
